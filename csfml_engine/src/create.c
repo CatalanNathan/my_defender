265b51a32b46ef411badbdb9c0b4455c969923d5
MisTraleuh <nathan.catalan@epitech.eu>
2022-02-22 10:44:31 +0100

    [FIXED] Mouvements des personnages

--- csfml_engine/src/create.c
+++ csfml_engine/src/create.c
@@ -14,7 +14,7 @@ void create_enemies(var_t *var)
     // static int i = 1090; // POSSIBILITÉ 2
     static int i = 400; //P OSSIBILITÉ 1
     // V2f *path = random_this((V2f[3]){{1015, 640}, {1290, 450}, {1000, 100}}, 3); // POSSIBILITÉ 2
-    V2f *path = random_this((V2f[5]){{555, 470}, {750, 550}, {925, 450}, {780, 355}, {1000, 100}}, 5); // POSSIBILITÉ 1
+    V2f *path = random_this((V2f[5]){{555, 470}, {750, 550}, {910, 450}, {780, 370}, {980, 100}}, 5); // POSSIBILITÉ 1
 
     var->enemies = create_object("player.png", "enemies", 9);
     set_shape_obj(&var->enemies, (sfIntRect){0, 48, 48, 48}, true);
